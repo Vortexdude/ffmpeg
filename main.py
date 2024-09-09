4208d1ffb03e15102ed7b97f3aa3a18facfb347b
vortexdude <nitinnamdeo456@gmail.com>
2024-09-09 17:00:41 +0530

    :trophy: update README.md

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
-from ffmpeg import FFMPEG
+# from ffmpeg import FFMPEG
 
 
 if __name__ == "__main__":
     INPUT_FILE = "dms.mkv"
 
     # Create a video processor instance and process the video
-    video_processor = FFMPEG(input_file=INPUT_FILE)
-    video_processor.width = 720
-    video_processor.height = 1080
-    video_processor.scale()
+    # video_processor = FFMPEG(input_file=INPUT_FILE)
+    # video_processor.width = 720
+    # video_processor.height = 1080
+    # video_processor.scale()
     # chapters = video_processor.chapters
     # video_processor.split_chapter(chapters)
     # video_processor.remove_audio()
