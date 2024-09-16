3a8b5f15cf136a0e059c16e2d2243fa43423428c
vortexdude <nitinnamdeo456@gmail.com>
2024-09-16 23:32:17 +0530

    :hammer: fix module imports 0.1.1

--- main.py
+++ main.py
@@ -1,20 +1,20 @@
 from ffmpeg import FFMPEG
 
-file = "dms.mkv"
+file = "sample.mp4"
 
 ss = FFMPEG(file_path=file)
 
 # chapters = ss.metadata.get('chapters')
-# print(chapters)
+# print(ss.metadata)
 # ss.split(chapters)
-# ss.remove_audio()
-# ss.take_screenshot()
-# audio_stream = ss.audio_streams
-# video_stream = ss.video_streams
+# ss.remove_audio(output_file="sample_out_with_no_audio.mp4")
+# ss.take_screenshot(time="00:00:12")
+audio_stream = ss.audio_streams
+video_stream = ss.video_streams
 # print(audio_stream)
 # for stream in audio_stream:
-#     if stream['name'] == "hin":
+#     if stream['name'] == "eng":
 #         ss.extrac_audio(stream)
 
-# ss.reverse_video(include_audio=False)
+ss.reverse_video(include_audio=False)
 ss.scale("480x240")
