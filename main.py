c15e08c6735c96c3f8ee0374c3e4b9912e7acb6d
vortexdude <nitinnamdeo456@gmail.com>
2024-09-19 23:37:56 +0530

    :bottle: added usage file

--- main.py
+++ main.py
@@ -1,23 +1,29 @@
 from ffmpeg import FFMPEG
 
-# file = "https://v1.pinimg.com/videos/mc/hls/8f/b1/d6/8fb1d693ed2890d1eb45fef1127f3fd0.m3u8"
 file = "sample.mp4"
+# file = "audio.mp3"
+# file = "tanjiro.jpeg"
+# file = "https://v1.pinimg.com/videos/mc/hls/8f/b1/d6/8fb1d693ed2890d1eb45fef1127f3fd0.m3u8"
 
 app = FFMPEG(input_stream=file)
-chapters = app.metadata.get('chapters')
+app.convert_to_gif(seek="00:02:16")
 
-if chapters:
-    app.split(chapters)
+# print(app.metadata)
 
-app.remove_audio(force_replace=True)
-app.take_screenshot(time="00:00:05", force_replace=True)
-audio_stream = app.audio_streams
-video_stream = app.video_streams
-
-for stream in audio_stream:
-    if stream['name'] == "eng":
-        app.extrac_audio(stream, force_replace=True)
+# chapters = app.metadata.get('chapters')
 #
-# ss.reverse_video(include_audio=False)
-app.scale("720x480", force_replace=True)
-app.convert_to_gif(force_replace=True)
+# if chapters:
+#     app.split(chapters)
+#
+# app.remove_audio(force_replace=True)
+# app.take_screenshot(time="00:00:05", force_replace=True)
+# audio_stream = app.audio_streams
+# video_stream = app.video_streams
+#
+# for stream in audio_stream:
+#     if stream['name'] == "eng":
+#         app.extrac_audio(stream, force_replace=True)
+# #
+# # ss.reverse_video(include_audio=False)
+# app.scale("720x480", force_replace=True)
+# app.convert_to_gif(force_replace=True)
